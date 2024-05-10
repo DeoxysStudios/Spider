cd55faadf1eb30fb7496fad4240b51c0e9e16999
Liam <brownliamm@gmail.com>
2024-05-10 17:03:16 -0400

    finish spider.html v1

--- spider.js
+++ spider.js
@@ -1,5 +1,44 @@
 let blocksTag = document.getElementById("blocks-indicator");
-let startDate = new Date("Tuesday, August 2, 2016 5:00:00 PM")
-let currDate = new Date()
+let startDate = new Date("Tuesday, August 2, 2016 5:00:00 PM");
+let currDate = new Date();
+let blocksFallen = 0;
+let timeout = null;
 
-blocksTag.textContent = `${(currDate - startDate) / 10}`;
+var Timer = function(callback, delay) {
+    var timerId, start, remaining = delay;
+    this.pause = function() {
+        window.clearTimeout(timerId);
+        timerId = null;
+        remaining -= Date.now() - start;
+    };
+    this.resume = function() {
+        if (timerId) {
+            return;
+        }
+        start = Date.now();
+        timerId = window.setTimeout(callback, remaining);
+    };
+    this.resume();
+};
+
+function update_site() {
+    currDate = new Date();
+    blocksFallen = Math.round((currDate - startDate) / 10);
+    blocksTag.textContent = add_commas(blocksFallen);
+    Timer(update_site, 10);
+}
+
+function add_commas(x) {
+    let outstr = ``;
+    let instr = `${x}`;
+    let n = instr.length;
+    for (let i = 1; i <= n; i++) {
+        outstr = instr[n - i] + outstr;
+        if (((i % 3) == 0) && (i != n)) {
+            outstr = `,` + outstr;
+        }
+    }
+    return outstr;
+}
+
+update_site();
